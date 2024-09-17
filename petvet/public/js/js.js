12eaf8fa282a6bd1111ca0d44772cd2be171bf11
mj23-coder <calupcupan.marcjustin.bsit@gmail.com>
2024-09-18 04:58:19 +0800

    etopla

--- petvet/public/js/js.js
+++ petvet/public/js/js.js
@@ -1,3 +1,12 @@
-document.querySelector('.img__btn').addEventListener('click', function() {
-    document.querySelector('.cont').classList.toggle('s--signup');
+document.addEventListener('DOMContentLoaded', function() {
+    const imgBtn = document.querySelector('.image-buttons');
+    const container = document.querySelector('.container');
+
+    if (imgBtn && container) {
+        imgBtn.addEventListener('click', function() {
+            container.classList.toggle('s--signup');
+        });
+    } else {
+        console.error('Elements not found');
+    }
 });
--- petvet/resources/views/welcome.blade.php
+++ petvet/resources/views/welcome.blade.php
@@ -1,62 +1,62 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
-        <title>VET TO</title>
-    </head>
-    <body >
-        <img class="pets" src="{{ asset('images/pet.jpg') }}" alt=""> 
-        <br>
-        <br>
-        <div class="cont">
-            <div class="form sign-in">
-                <h2>Welcome to PetVet</h2>
-                <label>
-                    <span>Email</span>
-                    <input type="email" />
-                </label>
-                <label>
-                    <span>Password</span>
-                    <input type="password" />
-                </label>
-                    <p class="forgot-pass">Forgot password?</p>
-                    <button type="button" class="submit">Sign In</button>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
+    <title>PetVet</title>
+</head>
+<body>
+    <img class="pet" src="{{ asset('images/pet.jpg') }}" alt="Hero Image"> 
+    <br>
+    <br>
+    <div class="container">
+        <div class="form-container form-sign-in">
+            <h2 class="form-title">Welcome to PetVet</h2>
+            <label class="form-label">
+                <span class="label-text">Email</span>
+                <input type="email" class="form-input" />
+            </label>
+            <label class="form-label">
+                <span class="label-text">Password</span>
+                <input type="password" class="form-input" />
+            </label>
+            <p class="forgot-password">Forgot password?</p>
+            <button type="button" class="form-button">Sign In</button>
+        </div>
+        <div class="sub-container">
+            <div class="image-container">
+                <div class="image-text image-text--up">
+                    <h3>Don't have an account? Please Sign Up!</h3>
+                    <img class="corgi-image" src="{{ asset('images/corgi.png') }}" alt="Corgi">
+                </div>
+                <div class="image-text image-text--in">
+                    <h3>If you already have an account, just sign in.</h3>
+                    <img class="corgi-image" src="{{ asset('images/corgi.png') }}" alt="Corgi">
+                </div>
+                <div class="image-buttons">
+                    <span class="image-button--up">Sign Up</span>
+                    <span class="image-button--in">Sign In</span>
+                </div>
             </div>
-            <div class="sub-cont">
-                <div class="img">
-                    <div class="img__text m--up">
-                        <h3>Don't have an account? Please Sign up!<h3>
-                        <img class="corgi" src="{{ asset('images/corgi.png') }}" alt="">
-                    </div>
-                    <div class="img__text m--in">
-                        <h3>If you already has an account, just sign in.<h3>
-                        <img class="corgi" src="{{ asset('images/corgi.png') }}" alt="">
-                    </div>
-                    <div class="img__btn">
-                        <span class="m--up">Sign Up</span>
-                        <span class="m--in">Sign In</span>
-                    </div>
-                </div>
-                <div class="form sign-up">
-                    <h2>Create your Account</h2>
-                    <label>
-                        <span>Name</span>
-                        <input type="text" />
-                    </label>
-                    <label>
-                        <span>Email</span>
-                        <input type="email" />
-                    </label>
-                    <label>
-                        <span>Password</span>
-                        <input type="password" />
-                    </label>
-                    <button type="button" class="submit">Sign Up</button>
-                </div>
+            <div class="form-container form-sign-up">
+                <h2 class="form-title">Create your Account</h2>
+                <label class="form-label">
+                    <span class="label-text">Name</span>
+                    <input type="text" class="form-input" />
+                </label>
+                <label class="form-label">
+                    <span class="label-text">Email</span>
+                    <input type="email" class="form-input" />
+                </label>
+                <label class="form-label">
+                    <span class="label-text">Password</span>
+                    <input type="password" class="form-input" />
+                </label>
+                <button type="button" class="form-button">Sign Up</button>
             </div>
         </div>
-        <script src="{{ asset('js/js.js') }}"></script>
-    </body>
+    </div>
+    <script src="{{ asset('js/js.js') }}"></script>
+</body>
 </html>
